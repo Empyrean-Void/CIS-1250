ebe2237d1cd58dad3a42fb0f256a08e369084b8b
Empyrean-Void <jshortz@proton.me>
2022-10-13 16:40:44 -0600

    Added more sequence examples

---
+++ examples/sequences/empty_list.py
@@ -0,0 +1,8 @@
+# Causes Error
+# empty = []
+# empty[5] = 55
+# print(empty)
+
+betterEmpty = [None] * 10
+betterEmpty[5] = 55
+print(betterEmpty)
---
+++ examples/sequences/list_func.py
@@ -0,0 +1,7 @@
+text = 'Hello'
+lst_1 = list(text)
+print(lst_1)
+
+my_tuple = (69, 'G Jones', 420, 'Eprom')
+lst_2 = list(my_tuple)
+print(lst_2)
